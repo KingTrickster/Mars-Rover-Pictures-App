9c9b0056862680d320cbd20e04905be6517a7be5
Alejandro Trejo <alejandro.trejo@enroutesystems.com>
2022-08-02 12:27:53 -0500

    Added Logos and header for frontend app

---
+++ frontend/src/components/header/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './header.css';
+import logoUrl from '../../assets/png/nasa_logo.png';
+const Header = () => (
+  <div className="header">
+     <img src={logoUrl} alt="Nasa" /> 
+  </div>
+)
+export default Header;

b44144859b323a0059ff29ae3747b3a6167f9b2f
Alejandro Trejo Reséndiz <92407519+KingTwice@users.noreply.github.com>
2022-08-04 15:11:48 -0500

    Added layout (#4)
    Co-authored-by: Alejandro Trejo <alejandro.trejo@enroutesystems.com>

---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,15 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+      <div className='footer-container'>
+        <footer>
+          <div>
+            <span>© 2022 Nasa</span>
+          </div>
+        </footer>
+      </div>
+    )
+  }
+  
+  export default Footer
